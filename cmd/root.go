23e6248359cc87c7b9238d4f02b459519b8d654a
Wickie Lee <wickie.leejr@gmail.com>
2020-05-24 13:42:06 -0400

    basic golang code generation

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -26,27 +29,74 @@ var (
 			if err != nil {
 				panic(err)
 			}
+			filePath, err := cmd.PersistentFlags().GetString("out")
+			var file *os.File
+			if filePath == "" {
+				file = os.Stdout
+			} else {
+				file, err = os.Create(filePath)
+				if err != nil {
+					panic(err)
+				}
+			}
+			if err != nil {
+				panic(err)
+			}
+			in, err := cmd.PersistentFlags().GetString("in")
+			if err != nil {
+				panic(err)
+			}
 			if format == "json" {
 				opcodes, err := i80.GenOpCodes()
 				if err != nil {
 					panic(err)
 				}
-				writeOpcodes("./out/opcodes.json", opcodes)
+				writeOpcodes(file, opcodes)
+			} else if format == "go" {
+				var opcodes []opcode.OpCode
+				if in == "" {
+					opcodes, err = i80.GenOpCodes()
+					if err != nil {
+						panic(err)
+					}
+				} else {
+					opcodes = readOpJSON(in)
+				}
+				writeGo(file, opcodes)
 			} else {
-				fmt.Println("Currently only supports JSON output")
+				fmt.Println("Currently only supports JSON and Go output")
 			}
 		},
 	}
 )
 
-func writeOpcodes(fileName string, codes []opcode.OpCode) {
-	f, err := os.Create(fileName)
+func readOpJSON(fileName string) []opcode.OpCode {
+	var ops []opcode.OpCode
+	b, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		panic(err)
 	}
-	enc := json.NewEncoder(f)
+	br := bytes.NewReader(b)
+	err = json.NewDecoder(br).Decode(&ops)
+	if err != nil {
+		panic(err)
+	}
+	return ops
+}
+
+func writeGo(file *os.File, codes []opcode.OpCode) {
+	goCode := "package opcode\n\nimport (\n\t\"fmt\"\n)\n\n// HandleOp handles opcodes\nfunc HandleOp(op byte) {\n\tswitch op {\n"
+	for _, op := range codes {
+		goCode += fmt.Sprintf("\tcase 0x%02X:\n\t\tfmt.Printf(\"Handling OpCode: 0x%02X\")\n", op.Code, op.Code)
+	}
+	goCode += "\t\tdefault:\n\t\tfmt.Printf(\"Unrecognized opcode: 0x%02X\", op)\n\t}\n}\n"
+	io.WriteString(file, goCode)
+}
+
+func writeOpcodes(file *os.File, codes []opcode.OpCode) {
+	enc := json.NewEncoder(file)
 	enc.SetEscapeHTML(false)
-	err = enc.Encode(codes)
+	err := enc.Encode(codes)
 	if err != nil {
 		panic(err)
 	}
@@ -58,5 +108,12 @@ func Execute() error {
 }
 
 func init() {
+	cobra.OnInitialize(initConfig)
 	rootCmd.PersistentFlags().StringP("format", "f", "json", "output format")
+	rootCmd.PersistentFlags().String("in", "", "input file")
+	rootCmd.PersistentFlags().String("out", "", "name of output file. defaults to std out")
+}
+
+func initConfig() {
+	return
 }
---
+++ opcode/handle.go
@@ -0,0 +1,525 @@
+package opcode
+
+import (
+	"fmt"
+)
+
+// HandleOp handles opcodes
+func HandleOp(op byte) {
+	switch op {
+	case 0x00:
+		fmt.Printf("Handling OpCode: 0x00")
+	case 0x01:
+		fmt.Printf("Handling OpCode: 0x01")
+	case 0x02:
+		fmt.Printf("Handling OpCode: 0x02")
+	case 0x03:
+		fmt.Printf("Handling OpCode: 0x03")
+	case 0x04:
+		fmt.Printf("Handling OpCode: 0x04")
+	case 0x05:
+		fmt.Printf("Handling OpCode: 0x05")
+	case 0x06:
+		fmt.Printf("Handling OpCode: 0x06")
+	case 0x07:
+		fmt.Printf("Handling OpCode: 0x07")
+	case 0x08:
+		fmt.Printf("Handling OpCode: 0x08")
+	case 0x09:
+		fmt.Printf("Handling OpCode: 0x09")
+	case 0x0A:
+		fmt.Printf("Handling OpCode: 0x0A")
+	case 0x0B:
+		fmt.Printf("Handling OpCode: 0x0B")
+	case 0x0C:
+		fmt.Printf("Handling OpCode: 0x0C")
+	case 0x0D:
+		fmt.Printf("Handling OpCode: 0x0D")
+	case 0x0E:
+		fmt.Printf("Handling OpCode: 0x0E")
+	case 0x0F:
+		fmt.Printf("Handling OpCode: 0x0F")
+	case 0x10:
+		fmt.Printf("Handling OpCode: 0x10")
+	case 0x11:
+		fmt.Printf("Handling OpCode: 0x11")
+	case 0x12:
+		fmt.Printf("Handling OpCode: 0x12")
+	case 0x13:
+		fmt.Printf("Handling OpCode: 0x13")
+	case 0x14:
+		fmt.Printf("Handling OpCode: 0x14")
+	case 0x15:
+		fmt.Printf("Handling OpCode: 0x15")
+	case 0x16:
+		fmt.Printf("Handling OpCode: 0x16")
+	case 0x17:
+		fmt.Printf("Handling OpCode: 0x17")
+	case 0x18:
+		fmt.Printf("Handling OpCode: 0x18")
+	case 0x19:
+		fmt.Printf("Handling OpCode: 0x19")
+	case 0x1A:
+		fmt.Printf("Handling OpCode: 0x1A")
+	case 0x1B:
+		fmt.Printf("Handling OpCode: 0x1B")
+	case 0x1C:
+		fmt.Printf("Handling OpCode: 0x1C")
+	case 0x1D:
+		fmt.Printf("Handling OpCode: 0x1D")
+	case 0x1E:
+		fmt.Printf("Handling OpCode: 0x1E")
+	case 0x1F:
+		fmt.Printf("Handling OpCode: 0x1F")
+	case 0x20:
+		fmt.Printf("Handling OpCode: 0x20")
+	case 0x21:
+		fmt.Printf("Handling OpCode: 0x21")
+	case 0x22:
+		fmt.Printf("Handling OpCode: 0x22")
+	case 0x23:
+		fmt.Printf("Handling OpCode: 0x23")
+	case 0x24:
+		fmt.Printf("Handling OpCode: 0x24")
+	case 0x25:
+		fmt.Printf("Handling OpCode: 0x25")
+	case 0x26:
+		fmt.Printf("Handling OpCode: 0x26")
+	case 0x27:
+		fmt.Printf("Handling OpCode: 0x27")
+	case 0x28:
+		fmt.Printf("Handling OpCode: 0x28")
+	case 0x29:
+		fmt.Printf("Handling OpCode: 0x29")
+	case 0x2A:
+		fmt.Printf("Handling OpCode: 0x2A")
+	case 0x2B:
+		fmt.Printf("Handling OpCode: 0x2B")
+	case 0x2C:
+		fmt.Printf("Handling OpCode: 0x2C")
+	case 0x2D:
+		fmt.Printf("Handling OpCode: 0x2D")
+	case 0x2E:
+		fmt.Printf("Handling OpCode: 0x2E")
+	case 0x2F:
+		fmt.Printf("Handling OpCode: 0x2F")
+	case 0x30:
+		fmt.Printf("Handling OpCode: 0x30")
+	case 0x31:
+		fmt.Printf("Handling OpCode: 0x31")
+	case 0x32:
+		fmt.Printf("Handling OpCode: 0x32")
+	case 0x33:
+		fmt.Printf("Handling OpCode: 0x33")
+	case 0x34:
+		fmt.Printf("Handling OpCode: 0x34")
+	case 0x35:
+		fmt.Printf("Handling OpCode: 0x35")
+	case 0x36:
+		fmt.Printf("Handling OpCode: 0x36")
+	case 0x37:
+		fmt.Printf("Handling OpCode: 0x37")
+	case 0x38:
+		fmt.Printf("Handling OpCode: 0x38")
+	case 0x39:
+		fmt.Printf("Handling OpCode: 0x39")
+	case 0x3A:
+		fmt.Printf("Handling OpCode: 0x3A")
+	case 0x3B:
+		fmt.Printf("Handling OpCode: 0x3B")
+	case 0x3C:
+		fmt.Printf("Handling OpCode: 0x3C")
+	case 0x3D:
+		fmt.Printf("Handling OpCode: 0x3D")
+	case 0x3E:
+		fmt.Printf("Handling OpCode: 0x3E")
+	case 0x3F:
+		fmt.Printf("Handling OpCode: 0x3F")
+	case 0x40:
+		fmt.Printf("Handling OpCode: 0x40")
+	case 0x41:
+		fmt.Printf("Handling OpCode: 0x41")
+	case 0x42:
+		fmt.Printf("Handling OpCode: 0x42")
+	case 0x43:
+		fmt.Printf("Handling OpCode: 0x43")
+	case 0x44:
+		fmt.Printf("Handling OpCode: 0x44")
+	case 0x45:
+		fmt.Printf("Handling OpCode: 0x45")
+	case 0x46:
+		fmt.Printf("Handling OpCode: 0x46")
+	case 0x47:
+		fmt.Printf("Handling OpCode: 0x47")
+	case 0x48:
+		fmt.Printf("Handling OpCode: 0x48")
+	case 0x49:
+		fmt.Printf("Handling OpCode: 0x49")
+	case 0x4A:
+		fmt.Printf("Handling OpCode: 0x4A")
+	case 0x4B:
+		fmt.Printf("Handling OpCode: 0x4B")
+	case 0x4C:
+		fmt.Printf("Handling OpCode: 0x4C")
+	case 0x4D:
+		fmt.Printf("Handling OpCode: 0x4D")
+	case 0x4E:
+		fmt.Printf("Handling OpCode: 0x4E")
+	case 0x4F:
+		fmt.Printf("Handling OpCode: 0x4F")
+	case 0x50:
+		fmt.Printf("Handling OpCode: 0x50")
+	case 0x51:
+		fmt.Printf("Handling OpCode: 0x51")
+	case 0x52:
+		fmt.Printf("Handling OpCode: 0x52")
+	case 0x53:
+		fmt.Printf("Handling OpCode: 0x53")
+	case 0x54:
+		fmt.Printf("Handling OpCode: 0x54")
+	case 0x55:
+		fmt.Printf("Handling OpCode: 0x55")
+	case 0x56:
+		fmt.Printf("Handling OpCode: 0x56")
+	case 0x57:
+		fmt.Printf("Handling OpCode: 0x57")
+	case 0x58:
+		fmt.Printf("Handling OpCode: 0x58")
+	case 0x59:
+		fmt.Printf("Handling OpCode: 0x59")
+	case 0x5A:
+		fmt.Printf("Handling OpCode: 0x5A")
+	case 0x5B:
+		fmt.Printf("Handling OpCode: 0x5B")
+	case 0x5C:
+		fmt.Printf("Handling OpCode: 0x5C")
+	case 0x5D:
+		fmt.Printf("Handling OpCode: 0x5D")
+	case 0x5E:
+		fmt.Printf("Handling OpCode: 0x5E")
+	case 0x5F:
+		fmt.Printf("Handling OpCode: 0x5F")
+	case 0x60:
+		fmt.Printf("Handling OpCode: 0x60")
+	case 0x61:
+		fmt.Printf("Handling OpCode: 0x61")
+	case 0x62:
+		fmt.Printf("Handling OpCode: 0x62")
+	case 0x63:
+		fmt.Printf("Handling OpCode: 0x63")
+	case 0x64:
+		fmt.Printf("Handling OpCode: 0x64")
+	case 0x65:
+		fmt.Printf("Handling OpCode: 0x65")
+	case 0x66:
+		fmt.Printf("Handling OpCode: 0x66")
+	case 0x67:
+		fmt.Printf("Handling OpCode: 0x67")
+	case 0x68:
+		fmt.Printf("Handling OpCode: 0x68")
+	case 0x69:
+		fmt.Printf("Handling OpCode: 0x69")
+	case 0x6A:
+		fmt.Printf("Handling OpCode: 0x6A")
+	case 0x6B:
+		fmt.Printf("Handling OpCode: 0x6B")
+	case 0x6C:
+		fmt.Printf("Handling OpCode: 0x6C")
+	case 0x6D:
+		fmt.Printf("Handling OpCode: 0x6D")
+	case 0x6E:
+		fmt.Printf("Handling OpCode: 0x6E")
+	case 0x6F:
+		fmt.Printf("Handling OpCode: 0x6F")
+	case 0x70:
+		fmt.Printf("Handling OpCode: 0x70")
+	case 0x71:
+		fmt.Printf("Handling OpCode: 0x71")
+	case 0x72:
+		fmt.Printf("Handling OpCode: 0x72")
+	case 0x73:
+		fmt.Printf("Handling OpCode: 0x73")
+	case 0x74:
+		fmt.Printf("Handling OpCode: 0x74")
+	case 0x75:
+		fmt.Printf("Handling OpCode: 0x75")
+	case 0x76:
+		fmt.Printf("Handling OpCode: 0x76")
+	case 0x77:
+		fmt.Printf("Handling OpCode: 0x77")
+	case 0x78:
+		fmt.Printf("Handling OpCode: 0x78")
+	case 0x79:
+		fmt.Printf("Handling OpCode: 0x79")
+	case 0x7A:
+		fmt.Printf("Handling OpCode: 0x7A")
+	case 0x7B:
+		fmt.Printf("Handling OpCode: 0x7B")
+	case 0x7C:
+		fmt.Printf("Handling OpCode: 0x7C")
+	case 0x7D:
+		fmt.Printf("Handling OpCode: 0x7D")
+	case 0x7E:
+		fmt.Printf("Handling OpCode: 0x7E")
+	case 0x7F:
+		fmt.Printf("Handling OpCode: 0x7F")
+	case 0x80:
+		fmt.Printf("Handling OpCode: 0x80")
+	case 0x81:
+		fmt.Printf("Handling OpCode: 0x81")
+	case 0x82:
+		fmt.Printf("Handling OpCode: 0x82")
+	case 0x83:
+		fmt.Printf("Handling OpCode: 0x83")
+	case 0x84:
+		fmt.Printf("Handling OpCode: 0x84")
+	case 0x85:
+		fmt.Printf("Handling OpCode: 0x85")
+	case 0x86:
+		fmt.Printf("Handling OpCode: 0x86")
+	case 0x87:
+		fmt.Printf("Handling OpCode: 0x87")
+	case 0x88:
+		fmt.Printf("Handling OpCode: 0x88")
+	case 0x89:
+		fmt.Printf("Handling OpCode: 0x89")
+	case 0x8A:
+		fmt.Printf("Handling OpCode: 0x8A")
+	case 0x8B:
+		fmt.Printf("Handling OpCode: 0x8B")
+	case 0x8C:
+		fmt.Printf("Handling OpCode: 0x8C")
+	case 0x8D:
+		fmt.Printf("Handling OpCode: 0x8D")
+	case 0x8E:
+		fmt.Printf("Handling OpCode: 0x8E")
+	case 0x8F:
+		fmt.Printf("Handling OpCode: 0x8F")
+	case 0x90:
+		fmt.Printf("Handling OpCode: 0x90")
+	case 0x91:
+		fmt.Printf("Handling OpCode: 0x91")
+	case 0x92:
+		fmt.Printf("Handling OpCode: 0x92")
+	case 0x93:
+		fmt.Printf("Handling OpCode: 0x93")
+	case 0x94:
+		fmt.Printf("Handling OpCode: 0x94")
+	case 0x95:
+		fmt.Printf("Handling OpCode: 0x95")
+	case 0x96:
+		fmt.Printf("Handling OpCode: 0x96")
+	case 0x97:
+		fmt.Printf("Handling OpCode: 0x97")
+	case 0x98:
+		fmt.Printf("Handling OpCode: 0x98")
+	case 0x99:
+		fmt.Printf("Handling OpCode: 0x99")
+	case 0x9A:
+		fmt.Printf("Handling OpCode: 0x9A")
+	case 0x9B:
+		fmt.Printf("Handling OpCode: 0x9B")
+	case 0x9C:
+		fmt.Printf("Handling OpCode: 0x9C")
+	case 0x9D:
+		fmt.Printf("Handling OpCode: 0x9D")
+	case 0x9E:
+		fmt.Printf("Handling OpCode: 0x9E")
+	case 0x9F:
+		fmt.Printf("Handling OpCode: 0x9F")
+	case 0xA0:
+		fmt.Printf("Handling OpCode: 0xA0")
+	case 0xA1:
+		fmt.Printf("Handling OpCode: 0xA1")
+	case 0xA2:
+		fmt.Printf("Handling OpCode: 0xA2")
+	case 0xA3:
+		fmt.Printf("Handling OpCode: 0xA3")
+	case 0xA4:
+		fmt.Printf("Handling OpCode: 0xA4")
+	case 0xA5:
+		fmt.Printf("Handling OpCode: 0xA5")
+	case 0xA6:
+		fmt.Printf("Handling OpCode: 0xA6")
+	case 0xA7:
+		fmt.Printf("Handling OpCode: 0xA7")
+	case 0xA8:
+		fmt.Printf("Handling OpCode: 0xA8")
+	case 0xA9:
+		fmt.Printf("Handling OpCode: 0xA9")
+	case 0xAA:
+		fmt.Printf("Handling OpCode: 0xAA")
+	case 0xAB:
+		fmt.Printf("Handling OpCode: 0xAB")
+	case 0xAC:
+		fmt.Printf("Handling OpCode: 0xAC")
+	case 0xAD:
+		fmt.Printf("Handling OpCode: 0xAD")
+	case 0xAE:
+		fmt.Printf("Handling OpCode: 0xAE")
+	case 0xAF:
+		fmt.Printf("Handling OpCode: 0xAF")
+	case 0xB0:
+		fmt.Printf("Handling OpCode: 0xB0")
+	case 0xB1:
+		fmt.Printf("Handling OpCode: 0xB1")
+	case 0xB2:
+		fmt.Printf("Handling OpCode: 0xB2")
+	case 0xB3:
+		fmt.Printf("Handling OpCode: 0xB3")
+	case 0xB4:
+		fmt.Printf("Handling OpCode: 0xB4")
+	case 0xB5:
+		fmt.Printf("Handling OpCode: 0xB5")
+	case 0xB6:
+		fmt.Printf("Handling OpCode: 0xB6")
+	case 0xB7:
+		fmt.Printf("Handling OpCode: 0xB7")
+	case 0xB8:
+		fmt.Printf("Handling OpCode: 0xB8")
+	case 0xB9:
+		fmt.Printf("Handling OpCode: 0xB9")
+	case 0xBA:
+		fmt.Printf("Handling OpCode: 0xBA")
+	case 0xBB:
+		fmt.Printf("Handling OpCode: 0xBB")
+	case 0xBC:
+		fmt.Printf("Handling OpCode: 0xBC")
+	case 0xBD:
+		fmt.Printf("Handling OpCode: 0xBD")
+	case 0xBE:
+		fmt.Printf("Handling OpCode: 0xBE")
+	case 0xBF:
+		fmt.Printf("Handling OpCode: 0xBF")
+	case 0xC0:
+		fmt.Printf("Handling OpCode: 0xC0")
+	case 0xC1:
+		fmt.Printf("Handling OpCode: 0xC1")
+	case 0xC2:
+		fmt.Printf("Handling OpCode: 0xC2")
+	case 0xC3:
+		fmt.Printf("Handling OpCode: 0xC3")
+	case 0xC4:
+		fmt.Printf("Handling OpCode: 0xC4")
+	case 0xC5:
+		fmt.Printf("Handling OpCode: 0xC5")
+	case 0xC6:
+		fmt.Printf("Handling OpCode: 0xC6")
+	case 0xC7:
+		fmt.Printf("Handling OpCode: 0xC7")
+	case 0xC8:
+		fmt.Printf("Handling OpCode: 0xC8")
+	case 0xC9:
+		fmt.Printf("Handling OpCode: 0xC9")
+	case 0xCA:
+		fmt.Printf("Handling OpCode: 0xCA")
+	case 0xCB:
+		fmt.Printf("Handling OpCode: 0xCB")
+	case 0xCC:
+		fmt.Printf("Handling OpCode: 0xCC")
+	case 0xCD:
+		fmt.Printf("Handling OpCode: 0xCD")
+	case 0xCE:
+		fmt.Printf("Handling OpCode: 0xCE")
+	case 0xCF:
+		fmt.Printf("Handling OpCode: 0xCF")
+	case 0xD0:
+		fmt.Printf("Handling OpCode: 0xD0")
+	case 0xD1:
+		fmt.Printf("Handling OpCode: 0xD1")
+	case 0xD2:
+		fmt.Printf("Handling OpCode: 0xD2")
+	case 0xD3:
+		fmt.Printf("Handling OpCode: 0xD3")
+	case 0xD4:
+		fmt.Printf("Handling OpCode: 0xD4")
+	case 0xD5:
+		fmt.Printf("Handling OpCode: 0xD5")
+	case 0xD6:
+		fmt.Printf("Handling OpCode: 0xD6")
+	case 0xD7:
+		fmt.Printf("Handling OpCode: 0xD7")
+	case 0xD8:
+		fmt.Printf("Handling OpCode: 0xD8")
+	case 0xD9:
+		fmt.Printf("Handling OpCode: 0xD9")
+	case 0xDA:
+		fmt.Printf("Handling OpCode: 0xDA")
+	case 0xDB:
+		fmt.Printf("Handling OpCode: 0xDB")
+	case 0xDC:
+		fmt.Printf("Handling OpCode: 0xDC")
+	case 0xDD:
+		fmt.Printf("Handling OpCode: 0xDD")
+	case 0xDE:
+		fmt.Printf("Handling OpCode: 0xDE")
+	case 0xDF:
+		fmt.Printf("Handling OpCode: 0xDF")
+	case 0xE0:
+		fmt.Printf("Handling OpCode: 0xE0")
+	case 0xE1:
+		fmt.Printf("Handling OpCode: 0xE1")
+	case 0xE2:
+		fmt.Printf("Handling OpCode: 0xE2")
+	case 0xE3:
+		fmt.Printf("Handling OpCode: 0xE3")
+	case 0xE4:
+		fmt.Printf("Handling OpCode: 0xE4")
+	case 0xE5:
+		fmt.Printf("Handling OpCode: 0xE5")
+	case 0xE6:
+		fmt.Printf("Handling OpCode: 0xE6")
+	case 0xE7:
+		fmt.Printf("Handling OpCode: 0xE7")
+	case 0xE8:
+		fmt.Printf("Handling OpCode: 0xE8")
+	case 0xE9:
+		fmt.Printf("Handling OpCode: 0xE9")
+	case 0xEA:
+		fmt.Printf("Handling OpCode: 0xEA")
+	case 0xEB:
+		fmt.Printf("Handling OpCode: 0xEB")
+	case 0xEC:
+		fmt.Printf("Handling OpCode: 0xEC")
+	case 0xED:
+		fmt.Printf("Handling OpCode: 0xED")
+	case 0xEE:
+		fmt.Printf("Handling OpCode: 0xEE")
+	case 0xEF:
+		fmt.Printf("Handling OpCode: 0xEF")
+	case 0xF0:
+		fmt.Printf("Handling OpCode: 0xF0")
+	case 0xF1:
+		fmt.Printf("Handling OpCode: 0xF1")
+	case 0xF2:
+		fmt.Printf("Handling OpCode: 0xF2")
+	case 0xF3:
+		fmt.Printf("Handling OpCode: 0xF3")
+	case 0xF4:
+		fmt.Printf("Handling OpCode: 0xF4")
+	case 0xF5:
+		fmt.Printf("Handling OpCode: 0xF5")
+	case 0xF6:
+		fmt.Printf("Handling OpCode: 0xF6")
+	case 0xF7:
+		fmt.Printf("Handling OpCode: 0xF7")
+	case 0xF8:
+		fmt.Printf("Handling OpCode: 0xF8")
+	case 0xF9:
+		fmt.Printf("Handling OpCode: 0xF9")
+	case 0xFA:
+		fmt.Printf("Handling OpCode: 0xFA")
+	case 0xFB:
+		fmt.Printf("Handling OpCode: 0xFB")
+	case 0xFC:
+		fmt.Printf("Handling OpCode: 0xFC")
+	case 0xFD:
+		fmt.Printf("Handling OpCode: 0xFD")
+	case 0xFE:
+		fmt.Printf("Handling OpCode: 0xFE")
+	case 0xFF:
+		fmt.Printf("Handling OpCode: 0xFF")
+		default:
+		fmt.Printf("Unrecognized opcode: 0x%02X", op)
+	}
+}
